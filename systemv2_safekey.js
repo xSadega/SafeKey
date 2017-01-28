9d5dc8fa7061b69b17d97c0cb25b7693141d63ac
xSadega <skydeviluke@gmail.com>
2017-01-28 14:42:09 +0700

    Add files via upload

---
+++ systemv2_safekey.js
@@ -0,0 +1,74 @@
+<style type='text/css'>
+/* Message Box Facebook by Rouf Sadega */
+#box-message {
+position:fixed !important;
+position:absolute;
+top:-1000px;
+left:50%;
+margin:0px 0px 0px -182px;
+width:292px;
+height:auto;
+padding:16px;
+background:#fff;
+font:normal Dosis, Georgia, Serif;
+color:#111;
+border:3px solid #333;
+-webkit-box-shadow:0px 1px 2px rgba(0,0,0,0.4);
+-moz-box-shadow:0px 1px 2px rgba(0,0,0,0.4);
+box-shadow:0px 1px 2px rgba(0,0,0,0.4);
+-webkit-border-radius:10px;
+-moz-border-radius:10px;
+border-radius:10px;
+}
+#box-message a.close {
+position:absolute;
+top:-10px;
+right:-10px;
+background:#fff;
+font:bold 16px Arial, Sans-Serif;
+text-decoration:none;
+line-height:22px;
+width:22px;
+text-align:center;
+color:#111;
+border:2px solid #333;
+-webkit-box-shadow:0px 1px 2px rgba(0,0,0,0.4);
+-moz-box-shadow:0px 1px 2px rgba(0,0,0,0.4);
+box-shadow:0px 1px 2px rgba(0,0,0,0.4);
+-webkit-border-radius:22px;
+-moz-border-radius:22px;
+border-radius:22px;
+cursor:pointer;
+}
+#twitterx {
+background: #EEF9FD;
+padding: 10px;
+text-align:center;
+border: 1px solid #C7DBE2;
+border-top: 0;
+}
+</style>
+<script type='text/javascript'> 
+$(window).bind("load", function() {
+// Animate Top Value When Page Loaded Completed
+$('#box-message').animate({top:"50px"}, 1000);
+// Remove Mailbox When Close Button On Click
+$('a.close').click(function() {
+$(this).parent().fadeOut();
+return false;
+});
+});
+</script>
+<div id='box-message'>
+<!-- HTML Start -->
+<center><a class="murub">Like Fanspage Kami<br/> Like Fanspage agar kami dapat update terus setiap hari</a> <br/> <br/> </center>
+<center>
+<iframe src="//www.facebook.com/plugins/likebox.php?href=https://www.facebook.com/xRKey.Inc&amp;width=292&amp;height=258&amp;colorscheme=light&amp;show_faces=true&amp;border_color&amp;stream=false&amp;header=false&amp;appId=371524026202784" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:215px;" allowtransparency="true"></iframe>
+</center>
+<!-- HTML End -->
+<div class="twitter">
+<!-- Twitter -->
+<center><iframe allowtransparency='true' class='twitter-follow-button twitter-follow-button-rendered' data-screen-name='Rouf Sadega' frameborder='0' id='twitter-widget-0' scrolling='no' src='http://platform.twitter.com/widgets/follow_button.3476cd70032ff6b94ecc9ea63ab78a8b.id.html#_=1442564562969&amp;dnt=false&amp;id=twitter-widget-0&amp;lang=id&amp;screen_name=Rouf_Sadega&amp;show_count=true&amp;show_screen_name=false&amp;size=l' style='position: static; visibility: visible; width: 165px; height: 30px;' title='Twitter Follow Button'/> </iframe></center>
+</div>
+<a class='close' href='#'>&times;</a>
+</div>
